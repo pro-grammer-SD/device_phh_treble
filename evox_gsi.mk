ce3b8fc469981758bc418d2d1674a06d378e2427
doze-off <viniciussadxxx@gmail.com>
2025-08-14 10:29:36 -0300

    Update evox_gsi.mk

--- evox_gsi.mk
+++ evox_gsi.mk
@@ -2,11 +2,7 @@ TARGET_GAPPS_ARCH := arm64
 include build/make/target/product/aosp_arm64.mk
 $(call inherit-product, device/phh/treble/base.mk)
 
-
 $(call inherit-product, device/phh/treble/evox.mk)
-$(call inherit-product, vendor/google/overlays/ThemeIcons/config.mk)
-#$(call inherit-product, vendor/pixel-framework/config.mk)
-$(call inherit-product, vendor/pixel-style/config/common.mk)
 
 PRODUCT_NAME := evox_gsi
 PRODUCT_DEVICE := tdgsi_arm64_ab
@@ -22,13 +18,9 @@ TARGET_PRODUCT_PROP += device/phh/treble/product.prop
 # Overwrite the inherited "emulator" characteristics
 PRODUCT_CHARACTERISTICS := device
 
-PRODUCT_PACKAGES +=
-
-PRODUCT_PACKAGES += apns-conf.xml
-
 WITH_ADB_INSECURE := true
-WITH_GMS := false
-TARGET_USES_PICO_GAPPS := false
+WITH_GMS := true
+TARGET_USES_PICO_GAPPS := true
 
 WITH_SU := false
 EVO_BUILD_TYPE := Unofficial
