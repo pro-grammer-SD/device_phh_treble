d42a36f5d2ab6ad3dcc751d6ba6c84d87cc111dc
doze-off <viniciussadxxx@gmail.com>
2025-08-15 01:20:37 -0300

    r

--- prebuilt/apex/Android.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := com.android.runtime
-LOCAL_SRC_FILES := com.android.runtime.apex
-LOCAL_MODULE_CLASS := APEX
-LOCAL_MODULE_PATH := $(TARGET_OUT)/apex
-LOCAL_PREBUILT_MODULE_FILE := $(LOCAL_SRC_FILES)
-include $(BUILD_PREBUILT)
