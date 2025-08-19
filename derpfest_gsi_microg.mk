98ed6f5041713c7cb94a58c76bbad2faed5123dd
Doze-off <viniciussadxxx@gmail.com>
2025-08-19 18:29:26 -0300

    add microg variant derpfest, changer values blur

---
+++ derpfest_gsi_microg.mk
@@ -0,0 +1,28 @@
+TARGET_GAPPS_ARCH := arm64
+include build/make/target/product/aosp_arm64.mk
+$(call inherit-product, device/phh/treble/base.mk)
+
+$(call inherit-product, vendor/microg/microg.mk)
+
+$(call inherit-product, device/phh/treble/derp.mk)
+
+PRODUCT_NAME := derpfest_gsi_microg
+PRODUCT_DEVICE := tdgsi_arm64_ab
+PRODUCT_BRAND := google
+PRODUCT_SYSTEM_BRAND := google
+PRODUCT_MANUFACTURER := google
+PRODUCT_SYSTEM_MANUFACTURER := google
+
+PRODUCT_MODEL := DerpFest GSI
+
+TARGET_PRODUCT_PROP += device/phh/treble/product.prop
+
+# Overwrite the inherited "emulator" characteristics
+PRODUCT_CHARACTERISTICS := device
+
+WITH_ADB_INSECURE := true
+
+WITH_GMS := false
+
+PRODUCT_EXTRA_VNDK_VERSIONS += 28 29
+
