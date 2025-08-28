570129438c0c214afa828961fc679a184653bf8a
Doze-off <viniciussadxxx@gmail.com>
2025-08-27 21:19:24 -0300

    added matrixx

---
+++ matrix.mk
@@ -0,0 +1,34 @@
+$(call inherit-product, vendor/lineage/config/common.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/BoardConfigSoong.mk)
+$(call inherit-product, vendor/lineage/config/BoardConfigLineage.mk)
+$(call inherit-product, device/lineage/sepolicy/common/sepolicy.mk)
+-include vendor/lineage/build/core/config.mk
+
+TARGET_NO_KERNEL_OVERRIDE := true
+TARGET_NO_KERNEL_IMAGE := true
+
+PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := false
+LOCAL_UNINSTALLABLE_MODULE := true
+
+SELINUX_IGNORE_NEVERALLOWS := true
+
+
+override BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+
+TARGET_BOOT_ANIMATION_RES := 720
+
+PRODUCT_BROKEN_VERIFY_USES_LIBRARIES := true
+
+# To include Gapps
+WITH_GMS := true
+
+# To Add cinematic wallpaer support (only supported in gapps build not in vanilla remove this flag for vanilla builds)
+TARGET_SUPPORTS_WALLEFFECT := true
+
+# To Add Bypass Charging Support (need to be adapted in DT and KT as well)
+BYPASS_CHARGE_SUPPORTED  := true
+
+
+
+
---
+++ matrixx_gsi.mk
@@ -0,0 +1,29 @@
+TARGET_GAPPS_ARCH := arm64
+include build/make/target/product/aosp_arm64.mk
+$(call inherit-product, device/phh/treble/base.mk)
+
+$(call inherit-product, device/phh/treble/matrix.mk)
+#include vendor/pixel-framework/config.mk
+#include vendor/google/pixel/config.mk
+
+PRODUCT_NAME := matrix_gsi
+PRODUCT_DEVICE := tdgsi_arm64_ab
+PRODUCT_BRAND := google
+PRODUCT_SYSTEM_BRAND := google
+PRODUCT_MANUFACTURER := google
+PRODUCT_SYSTEM_MANUFACTURER := google
+
+PRODUCT_MODEL := Matrixx Treble
+
+TARGET_PRODUCT_PROP += device/phh/treble/product.prop
+
+# Overwrite the inherited "emulator" characteristics
+PRODUCT_CHARACTERISTICS := device
+
+WITH_ADB_INSECURE := true
+
+PRODUCT_EXTRA_VNDK_VERSIONS += 28 29
+
+
+
+#remove makupgoogle
