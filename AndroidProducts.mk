059327a9017e2fc6e9cf10584855d861f1dabaa9
Doze-off <viniciussadxxx@gmail.com>
2025-08-29 22:38:14 -0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -4,6 +4,7 @@ PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/infinity_a64_vanilla.mk \
 	$(LOCAL_DIR)/derpfest_gsi_gapps.mk \
 	$(LOCAL_DIR)/derpfest_gsi_microg.mk \
+	$(LOCAL_DIR)/derpfest_gsi_vanilla.mk \
 	$(LOCAL_DIR)/derpfest_a64_vanilla.mk \
 	$(LOCAL_DIR)/witaqua_gsi_gapps.mk \
 	$(LOCAL_DIR)/witaqua_gsi_vanilla.mk \
