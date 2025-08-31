0e14ba04a6dab382b2982dbe3a15851e4be8ebfe
Doze-off <viniciussadxxx@gmail.com>
2025-08-31 03:02:47 -0300

    fixed blur and disable back

--- phh-prop-handler.sh
+++ phh-prop-handler.sh
@@ -344,30 +344,47 @@ if [ "$1" == "persist.sys.phh.sf.background_blur" ]; then
         exit 1
     fi
 
-    if [[ "$prop_value" == disabled ]]; then
+    if [[ "$prop_value" == "disabled" ]]; then
         resetprop_phh ro.surface_flinger.supports_background_blur 0
         settings put global disable_window_blurs 1
         resetprop_phh --delete debug.renderengine.blur_algorithm
     fi
 
-    if [[ "$prop_value" == gaussian ]]; then
+    if [[ "$prop_value" == "gaussian" ]]; then
         resetprop_phh ro.surface_flinger.supports_background_blur 1
         settings put global disable_window_blurs 0
         resetprop_phh debug.renderengine.blur_algorithm gaussian
     fi
 
-    if [[ "$prop_value" == kawase ]]; then
+    if [[ "$prop_value" == "kawase" ]]; then
         resetprop_phh ro.surface_flinger.supports_background_blur 1
-        settings put global disable_window_blurs 1
+        settings put global disable_window_blurs 0
         resetprop_phh debug.renderengine.blur_algorithm kawase
     fi
 
-    if [[ "$prop_value" == kawase2 ]]; then
+    if [[ "$prop_value" == "kawase2" ]]; then
         resetprop_phh ro.surface_flinger.supports_background_blur 1
-        settings put global disable_window_blurs 1
+        settings put global disable_window_blurs 0
         resetprop_phh debug.renderengine.blur_algorithm kawase2
     fi
 
+    exit
+fi
+
+if [ "$1" == "debug.renderengine.backend" ]; then
+    if [[ "$prop_value" == "" ]]; then
+        resetprop_phh debug.renderengine.backend ""
+    elif [[ "$prop_value" == "skiagl" ]]; then
+        resetprop_phh debug.renderengine.backend skiagl
+    elif [[ "$prop_value" == "skiaglthreaded" ]]; then
+        resetprop_phh debug.renderengine.backend skiaglthreaded
+    elif [[ "$prop_value" == "skiavk" ]]; then
+        resetprop_phh debug.renderengine.backend skiavk
+    elif [[ "$prop_value" == "skiavkthreaded" ]]; then
+        resetprop_phh debug.renderengine.backend skiavkthreaded
+    else
+        exit 1
+    fi
     exit
 fi
 
