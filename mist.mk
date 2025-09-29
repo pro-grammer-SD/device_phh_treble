6ce978cef79b5bccc0f6594824644fa024c23906
Doze-off <viniciussadxxx@gmail.com>
2025-09-29 00:51:34 -0300

    jj

---
+++ mist.mk
@@ -0,0 +1,41 @@
+$(call inherit-product, vendor/lineage/config/common.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/BoardConfigSoong.mk)
+$(call inherit-product, vendor/lineage/config/BoardConfigLineage.mk)
+$(call inherit-product, device/lineage/sepolicy/common/sepolicy.mk)
+-include vendor/lineage/build/core/config.mk
+
+TARGET_NO_KERNEL_OVERRIDE := true
+TARGET_NO_KERNEL_IMAGE := true
+
+PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := false
+LOCAL_UNINSTALLABLE_MODULE := true
+
+SELINUX_IGNORE_NEVERALLOWS := true
+
+
+override BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+
+TARGET_BOOT_ANIMATION_RES := 1080
+
+TARGET_ENABLE_BLUR := true
+TARGET_DISABLE_EPPE := true
+TARGET_OPTIMIZED_DEXOPT := true
+
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.paranoid.maintainer=Doze-off
+
+WITH_BCR := true
+WITH_GMS := true
+TARGET_USES_OMNI_GAPPS := true
+# WITH_GMS := false
+
+PRODUCT_USE_SCUDO := true
+
+TARGET_DEFAULT_PIXEL_LAUNCHER := false
+
+PRODUCT_BROKEN_VERIFY_USES_LIBRARIES := true
+
+
+
+
--- mistos_gsi.mk
+++ mistos_gsi.mk
@@ -2,28 +2,22 @@ TARGET_GAPPS_ARCH := arm64
 include build/make/target/product/aosp_arm64.mk
 $(call inherit-product, device/phh/treble/base.mk)
 
-# $(call inherit-product, vendor/gapps/arm64/arm64-vendor.mk)
+$(call inherit-product, device/phh/treble/mist.mk)
 
-$(call inherit-product, device/phh/treble/yaap.mk)
-
-PRODUCT_NAME := yaap_gsi
+PRODUCT_NAME := mistos_gsi
 PRODUCT_DEVICE := tdgsi_arm64_ab
 PRODUCT_BRAND := google
 PRODUCT_SYSTEM_BRAND := google
 PRODUCT_MANUFACTURER := google
 PRODUCT_SYSTEM_MANUFACTURER := google
 
-PRODUCT_MODEL := YAAP Treble
+PRODUCT_MODEL := MIst-OS GSI
 
 TARGET_PRODUCT_PROP += device/phh/treble/product.prop
 
 # Overwrite the inherited "emulator" characteristics
 PRODUCT_CHARACTERISTICS := device
 
-WITH_ADB_INSECURE := true
-
 PRODUCT_EXTRA_VNDK_VERSIONS += 28 29
 
-
-
-#remove makupgoogle
+WITH_ADB_INSECURE := true
