38b0830e18acd2cdddebc2ceb9b2613146a50be9
Doze-off <viniciussadxxx@gmail.com>
2025-10-29 00:33:09 -0300

    Update lineage_gsi.mk

--- lineage_gsi.mk
+++ lineage_gsi.mk
@@ -2,6 +2,8 @@ TARGET_GAPPS_ARCH := arm64
 include build/make/target/product/aosp_arm64.mk
 $(call inherit-product, device/phh/treble/base.mk)
 
+# $(call inherit-product, vendor/gapps/arm64/arm64-vendor.mk)
+# $(call inherit-product, vendor/microg/microg.mk)
 
 $(call inherit-product, device/phh/treble/lineage.mk)
 
