e60946889584f0d5ff4bcfd039e77e711527e8db
Doze-off <viniciussadxxx@gmail.com>
2025-11-06 01:18:37 -0300

    added avium

---
+++ avium.mk
@@ -0,0 +1,39 @@
+$(call inherit-product, vendor/lineage/config/common.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/BoardConfigSoong.mk)
+$(call inherit-product, vendor/lineage/config/BoardConfigLineage.mk)
+$(call inherit-product, device/lineage/sepolicy/common/sepolicy.mk)
+-include vendor/lineage/build/core/config.mk
+
+TARGET_NO_KERNEL_OVERRIDE := true
+TARGET_NO_KERNEL_IMAGE := true
+
+PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := false
+LOCAL_UNINSTALLABLE_MODULE := true
+
+SELINUX_IGNORE_NEVERALLOWS := true
+
+
+override BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+
+
+
+PERF_ANIM_OVERRIDE := true
+
+TARGET_SCREEN_WIDTH := 1080
+TARGET_SCREEN_HEIGHT := 1920
+
+AVIUM_BUILDTYPE := Unoffical
+AVIUM_VERSION_APPEND_TIME_OF_DAY := false
+
+# GMS
+WITH_GMS := true
+TARGET_GMS_TYPE := PICO
+
+# Google IMS
+TARGET_INCLUDE_GOOGLEIME := false
+TARGET_GOOGLEIME_OVERRIDE_IME := false
+
+AVIUM_MAINTAINER := Doze-off
+TARGET_DISABLE_EPPE := true
+
