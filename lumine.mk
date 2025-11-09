063ae9cd65bfd485c0777d57bb76397e8767bb56
Doze-off <viniciussadxxx@gmail.com>
2025-11-09 17:31:48 -0300

    added luminedroid

---
+++ lumine.mk
@@ -0,0 +1,32 @@
+$(call inherit-product, vendor/lineage/config/common.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/BoardConfigSoong.mk)
+$(call inherit-product, vendor/lineage/config/BoardConfigSuperior.mk)
+$(call inherit-product, device/lineage/sepolicy/common/sepolicy.mk)
+-include vendor/lineage/build/core/config.mk
+
+TARGET_NO_KERNEL_OVERRIDE := true
+TARGET_NO_KERNEL_IMAGE := true
+
+PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := false
+LOCAL_UNINSTALLABLE_MODULE := true
+
+SELINUX_IGNORE_NEVERALLOWS := true
+
+
+override BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+
+
+
+PERF_ANIM_OVERRIDE := true
+
+TARGET_SCREEN_WIDTH := 1080
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_BOOT_ANIMATION_RES := 1080
+# GMS
+WITH_GMS := true
+
+TARGET_DISABLE_EPPE := true
+
+LUMINE_MAINTAINER := Doze-off
+
