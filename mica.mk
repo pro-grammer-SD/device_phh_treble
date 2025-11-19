bf00424f1ebb0f5e97fb38acffb3fa34151c3902
Doze-off <viniciussadxxx@gmail.com>
2025-11-19 01:03:40 -0300

    mica added

---
+++ mica.mk
@@ -0,0 +1,28 @@
+$(call inherit-product, vendor/mica/config/common.mk)
+$(call inherit-product, vendor/mica/config/common_full_phone.mk)
+$(call inherit-product, vendor/mica/config/BoardConfigSoong.mk)
+$(call inherit-product, vendor/mica/config/BoardConfigMica.mk)
+$(call inherit-product, device/lineage/sepolicy/common/sepolicy.mk)
+-include vendor/mica/build/core/config.mk
+
+TARGET_NO_KERNEL_OVERRIDE := true
+TARGET_NO_KERNEL_IMAGE := true
+
+PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := false
+LOCAL_UNINSTALLABLE_MODULE := true
+
+SELINUX_IGNORE_NEVERALLOWS := true
+
+
+override BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+
+
+
+PERF_ANIM_OVERRIDE := true
+
+TARGET_SCREEN_WIDTH := 1080
+TARGET_SCREEN_HEIGHT := 1920
+
+
+TARGET_DISABLE_EPPE := true
+
